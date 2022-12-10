794c79976249de3a8881882ffb17616a71eaa29f
Ken-Shu <t3636873@gmail.com>
2022-12-10 11:51:14 +0800

    2022/12/10 LiveData_fragment

---
+++ app_livedata_fragment/src/main/java/com/test/app_livedata_fragment/TwoFragment.java
@@ -0,0 +1,50 @@
+package com.test.app_livedata_fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModelProviders;
+
+public class TwoFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View parentView = inflater.inflate(R.layout.fragment_two, container, false);
+        SeekBar seekBar = parentView.findViewById(R.id.seekBar);
+        // 建立 ViewModel
+        ShareDataViewModel shareDataViewModel = ViewModelProviders.of(getActivity()).get(ShareDataViewModel.class);
+        // 建立 liveData
+        MutableLiveData<Integer> progressLiveData = (MutableLiveData<Integer>) shareDataViewModel.getProgress();
+        //監聽
+        progressLiveData.observe(getActivity(), (progress)-> {
+            seekBar.setProgress(progress);
+        });
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                Log.i("MyLog","Fragment One seeBar value = " + progress);
+                // 變更 livedata 的資料
+                progressLiveData.setValue(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        return parentView;
+    }
+}
