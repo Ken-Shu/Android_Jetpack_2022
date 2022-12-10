c6fd9d61e2dcb8e2af3fcd8f1998645a8c6a7d42
Ken-Shu <t3636873@gmail.com>
2022-12-10 10:29:06 +0800

    2022/12/10 LiveData

---
+++ app_livedata/src/main/java/com/test/app_livedata/TimerWithLiveDataViewModel.java
@@ -0,0 +1,45 @@
+package com.test.app_livedata;
+
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimerWithLiveDataViewModel extends ViewModel {
+
+    private Timer timer;
+
+    private MutableLiveData<Integer> currentSecond;
+
+    public LiveData<Integer> getCurrentSecond(){
+        if(currentSecond == null) {
+            currentSecond = new MutableLiveData<>();
+        }
+        return currentSecond;
+    }
+
+    public void startTiming(){
+        if(timer == null){
+            currentSecond.setValue(0);
+            timer = new Timer();
+            TimerTask timerTask = new TimerTask() {
+                @Override
+                public void run() {
+                    // 更新用 postValue
+                    currentSecond.postValue(currentSecond.getValue() + 1);
+                }
+            };
+            timer.schedule(timerTask, 1000 , 1000);
+        }
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        timer.cancel();
+    }
+}
